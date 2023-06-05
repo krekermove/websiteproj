3d51541c4301c58ecd858d70f16c20a7024ac33c
BurMinistr <papanoffk@ya.ru>
2023-06-05 09:34:26 +0300

    Добавил фронта.

--- casino/static/casino/js/door.js
+++ casino/static/casino/js/door.js
@@ -9,25 +9,31 @@ function chooseDoor(max) {
   door = Math.round(Math.random() * max);
 }
 
-function itIsRealDoor() {
-  alert("WIN");
-  document.location.href = casinoRedirect;
-}
-
-function fakeDoor() {
-  alert("LOSE")
-  document.location.href = casinoRedirect;
+function someDoor(id) {
+  if(id == door){
+    document.getElementById("textMessage").textContent = 'LOSE!'
+    document.getElementById(id).classList.add('lose');
+  }
+  else{
+    document.getElementById("textMessage").textContent = 'WIN!'
+    document.getElementById(id).classList.add('win');
+  }
+  setTimeout(function() {
+    document.location.href = casinoRedirect;
+  }, 3000)
 }
 
 function play(min, max) {
   document.getElementById("play").style = 'display: none;';
+  document.getElementById("listDoor").style = 'display: none;';
   chooseDoor(level-1);
   alert(door);
   for (let i = 0; i < level; i++){
     let newDoor = document.createElement('div');
+    newDoor.id = i;
     newDoor.classList.add('door');
-    newDoor.style = `left: ${300+i*100}px`
-    newDoor.addEventListener("click", (i==door) ? fakeDoor : itIsRealDoor );
+    newDoor.style = `left: ${300+i*300}px`;
+    newDoor.addEventListener("click", () => someDoor(newDoor.id));
     document.getElementById("game").appendChild(newDoor);
   }
 }
