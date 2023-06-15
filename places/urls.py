13837a952a6de401047d188f7eca2865b669bd7d
Rote <danilnikulin5336@gmail.com>
2023-06-16 00:20:40 +0300

    Карта с меткой

--- places/urls.py
+++ places/urls.py
@@ -7,7 +7,6 @@ from .views import PlacesDetailView
 urlpatterns = [
 	path('add', views.add, name='add'),
 	path('<int:pk>', PlacesDetailView.as_view(), name='place_detail'),
-	#path('map', views.maps, name='maps')
 	path(r'^delete/(?P<pk>\d+)/$', views.DeleteView.as_view(), name='delete_view'),
 	path('edit/<int:pk>', views.edit, name='edit_view'),
 ]
