6351076a00e9c094c709549d6adbeede1e606cc4
Rote <danilnikulin5336@gmail.com>
2023-06-15 16:33:30 +0300

    Ограничитель по координатам

--- places/models.py
+++ places/models.py
@@ -3,6 +3,7 @@ import sys, os
 sys.path.append(os.path.abspath('../accounts.models'))
 from accounts.models import User
 from django.urls import reverse
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 DEADLINE_CHOICES = (
     ('','Выбрать категорию...'),
@@ -14,12 +15,12 @@ DEADLINE_CHOICES = (
 class Places(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField('Название', max_length=50)
-    text = models.TextField('Описание')
+    text = models.TextField('Описание', max_length=300)
     slug = models.SlugField(null=False, unique=False)
     date = models.DateField(blank=True, null=True)
     deadline = models.CharField(max_length=20, choices=DEADLINE_CHOICES, default="Выбрать категорию...")
-    lattitude = models.CharField('Широта',max_length=20)
-    longtitude = models.CharField('Долгота',max_length=20)
+    lattitude = models.FloatField('Широта',max_length=20,validators=[MinValueValidator(59.8),MaxValueValidator(60)])
+    longtitude = models.FloatField('Долгота',max_length=20,validators=[MinValueValidator(30.147),MaxValueValidator(30.556)])
 
     def __str__(self):
         return self.title
